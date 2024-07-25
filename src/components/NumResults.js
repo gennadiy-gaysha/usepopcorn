7f33273a60b849b6bb320f494be0709d9b8f3f37
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-07-25 20:41:36 +0200

    refactor: split App.js component into smaller components

---
+++ src/components/NumResults.js
@@ -0,0 +1,10 @@
+function NumResults() {
+  return (
+    <p className="num-results">
+      {/* Found <strong>{movies.length}</strong> results */}
+      Found <strong>X</strong> results
+    </p>
+  );
+}
+
+export default NumResults;
