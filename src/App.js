98089221d83f08fa4e7ddfd291db4ade9c5bcfae
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-07-27 13:23:16 +0200

    feat: add callback with async func to useEffect to fetch data from api

--- src/App.js
+++ src/App.js
@@ -16,10 +16,29 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
 
+  // const handleClick = () => {
+  //   fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //     .then((res) => res.json())
+  //     .then((data) => setMovies(data.Search));
+  // };
+
+  // useEffect(function () {
+  //   fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //     .then((res) => res.json())
+  //     .then((data) => setMovies(data.Search));
+  //   return () => console.log("Cleanup");
+  // }, []);
+
+  // async function fetchMovies(): This defines an asynchronous function named fetchMovies inside the useEffect callback. This function will handle fetching the data from the API.
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
@@ -31,6 +50,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
+          {/* <button onClick={handleClick}>click!</button> */}
           <MovieList movies={movies} />
         </Box>
         <Box>
