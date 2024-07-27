2821d838e7bbc2cabee8d9865c8de47c46ce4750
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-07-27 11:40:33 +0200

    feat: add useEffect hook to fetch external API data on mount

--- src/App_v1.js
+++ src/App_v1.js
@@ -3,24 +3,16 @@ import Main from "./components/Main";
 import Logo from "./components/Logo";
 import Search from "./components/Search";
 import NumResults from "./components/NumResults";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { tempMovieData, tempWatchedData } from "./components/temporaryData";
 import Box from "./components/Box";
 import MovieList from "./components/MovieList";
 import WatchedSummary from "./components/WatchedSummary";
 import WatchedMovieList from "./components/WatchedMovieList";
 
-const KEY = "66198b53";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
-  }, []);
 
   return (
     <>
