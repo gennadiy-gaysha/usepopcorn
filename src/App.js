0b8a38972b73ae5e0ab0ce8c057fd7f118dcd885
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-07-28 14:40:01 +0200

    feat: add handle errors functionality for fetching API

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MovieList from "./components/MovieList";
 import WatchedSummary from "./components/WatchedSummary";
 import WatchedMovieList from "./components/WatchedMovieList";
 import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
 
 const KEY = "66198b53";
 
@@ -17,6 +18,7 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   // const handleClick = () => {
   //   fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
@@ -32,15 +34,54 @@ export default function App() {
   // }, []);
 
   // async function fetchMovies(): This defines an asynchronous function named fetchMovies inside the useEffect callback. This function will handle fetching the data from the API.
+
+  // const query = "interstellar";
+  const query = "klkjlk";
+
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`
-      );
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        );
+
+        // Cases Where !res.ok Triggers the Error:
+        // Client Errors (4xx): These include errors like 400 Bad Request, 401 Unauthorized, 403 Forbidden, 404 Not Found, etc.
+        // Server Errors (5xx): These include errors like 500 Internal Server Error, 502 Bad Gateway, 503 Service Unavailable, etc.
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movies");
+        const data = await res.json();
+
+        if (data.Response === "False")
+          throw new Error("Movies with this keyword is not found");
+
+        setMovies(data.Search);
+
+        // When an error is thrown within the try block, it is passed to the catch block as err.
+      } catch (err) {
+        // catches error, when internet connection is failed
+
+        // const customError = new Error(
+        //   "Network error: Please check your internet connection."
+        // );
+        // if (err.message === "Failed to fetch") {
+        //   console.error(customError.message);
+        //   setError(customError.message);
+
+        if (err.message === "Failed to fetch") {
+          console.error(
+            "Network error: Please check your internet connection."
+          );
+          setError(err.message);
+          // Errors instances from the try block go here
+        } else {
+          console.error(err.message);
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
@@ -55,7 +96,10 @@ export default function App() {
       <Main>
         <Box>
           {/* <button onClick={handleClick}>click!</button> */}
-          {isLoading ? <Loader /> : <MovieList movies={movies} />}
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {isLoading && <Loader />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
